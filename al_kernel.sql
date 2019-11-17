1b3324370437f9ffe9d406d9bc8fff65ac55d8cf
GrapsZ <live@gta-fivelife.fr>
2019-11-17 14:54:46 +0100

    update readme

--- al_kernel.sql
+++ al_kernel.sql
@@ -3,7 +3,7 @@ USE `essentialmode`;
 ALTER TABLE `users`
 	ADD COLUMN `name` VARCHAR(50) NULL DEFAULT '' AFTER `money`,
 	ADD COLUMN `skin` LONGTEXT NULL AFTER `name`,
-	ADD COLUMN `job` VARCHAR(50) NULL DEFAULT 'unemployed' AFTER `skin`,
+	ADD COLUMN `job` VARCHAR(50) NULL DEFAULT 'chomeur' AFTER `skin`,
 	ADD COLUMN `job_grade` INT NULL DEFAULT 0 AFTER `job`,
 	ADD COLUMN `loadout` LONGTEXT NULL AFTER `job_grade`,
 	ADD COLUMN `position` VARCHAR(36) NULL AFTER `loadout`
@@ -12,6 +12,7 @@ ALTER TABLE `users`
 CREATE TABLE `items` (
 	`name` VARCHAR(50) NOT NULL,
 	`label` VARCHAR(50) NOT NULL,
+	`limit` INT(11) NOT NULL,
 	`weight` INT(11) NOT NULL DEFAULT 1,
 	`rare` TINYINT(1) NOT NULL DEFAULT 0,
 	`can_remove` TINYINT(1) NOT NULL DEFAULT 1,
@@ -32,7 +33,7 @@ CREATE TABLE `job_grades` (
 	PRIMARY KEY (`id`)
 );
 
-INSERT INTO `job_grades` VALUES (1,'unemployed',0,'unemployed','Unemployed',200,'{}','{}');
+INSERT INTO `job_grades` VALUES (1,'chomeur',0,'chomeur','Chomeur',0,'{}','{}');
 
 CREATE TABLE `jobs` (
 	`name` VARCHAR(50) NOT NULL,
@@ -41,7 +42,7 @@ CREATE TABLE `jobs` (
 	PRIMARY KEY (`name`)
 );
 
-INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
+INSERT INTO `jobs` VALUES ('chomeur','Chomeur');
 
 CREATE TABLE `user_accounts` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
